fd078fd3012c8cf3a242a135e54c24ee194f7dc9
VladMeh <vladmeh@gmail.com>
2018-05-21 16:26:41 +0300

    init project

---
+++ src/test/java/com/vladmeh/graduation/GraduationTopjavaApplicationTest.java
@@ -0,0 +1,15 @@
+package com.vladmeh.graduation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+public class GraduationTopjavaApplicationTest {
+    @Test
+    public void contextLoads() {
+    }
+
+}
