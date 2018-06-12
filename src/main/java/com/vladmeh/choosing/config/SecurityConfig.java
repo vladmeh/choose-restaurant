f52a201be95e9928b3e3f83eb6832575045b8cf1
vladmeh <vladmeh@gmail.com>
2018-06-12 03:36:02 +0300

    fix_auth_api_users

--- src/main/java/com/vladmeh/choosing/config/SecurityConfig.java
+++ src/main/java/com/vladmeh/choosing/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/api/users**").hasAuthority(Role.ROLE_ADMIN.getAuthority())
+                .antMatchers("/api/users/**").hasAuthority(Role.ROLE_ADMIN.getAuthority())
                 .antMatchers("/api/**").authenticated()
                 .and()
                 .httpBasic()
