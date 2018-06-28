c38d0aa28f3fc5d580440068b3112fb14ff70de1
VladMeh <vladmeh@gmail.com>
2018-06-28 14:59:19 +0300

    fix_delete_restaurant

--- src/main/java/com/vladmeh/choosing/model/Restaurant.java
+++ src/main/java/com/vladmeh/choosing/model/Restaurant.java
@@ -3,7 +3,6 @@ package com.vladmeh.choosing.model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 import java.util.List;
@@ -14,14 +13,13 @@ import java.util.List;
  */
 
 @EqualsAndHashCode(callSuper = true)
-@Data
 @NoArgsConstructor
+@Data
 @Entity
 @Table(name = "restaurant", uniqueConstraints = {@UniqueConstraint(columnNames = "name", name = "unique_restaurant_name_idx")})
 public class Restaurant extends AbstractNamedEntity {
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "restaurant")
-    @Where(clause = "lunch_date = today()")
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "restaurant", cascade = CascadeType.REMOVE, orphanRemoval = true)
     private List<Lunch> lunch;
 
     public Restaurant(Long id, String name) {
